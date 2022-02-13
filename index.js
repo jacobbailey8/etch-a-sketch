a898f7389a9c16fa94cbe680ee4e69cef1840d0f
jacobbailey8 <jbaileydsi87@gmail.com>
2022-02-13 14:58:55 -0500

    Added reset button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let container = document.querySelector('.container');
-
+const container = document.querySelector('.container');
 let number = parseInt(prompt("Enter the number of boxes"));
+
 let boxWidth = 400 / number;
 
 for (let i = 0; i < (number ** 2); i++){
@@ -10,7 +10,6 @@ for (let i = 0; i < (number ** 2); i++){
     div.style.height = boxWidth + "px";
     container.appendChild(div);
 }
-
 let boxList = document.querySelectorAll('.container div');
 
 boxList.forEach((box) => {
@@ -19,5 +18,33 @@ boxList.forEach((box) => {
     });
 });
 
+function resetGrid(){
+    boxList.forEach(box => {
+        box.remove();
+    });
+
+    number = parseInt(prompt("Enter the number of boxes"));
+    boxWidth = 400 / number;
+
+    for (let i = 0; i < (number ** 2); i++){
+        let div = document.createElement('div');
+        div.classList.add('inside-box');
+        div.style.width = boxWidth + "px";
+        div.style.height = boxWidth + "px";
+        container.appendChild(div);
+    }
+
+    boxList = document.querySelectorAll('.container div');
+
+    boxList.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = '#b9b9ba';
+        });
+    });
+
+}
+
+const button = document.querySelector('.btn');
+button.addEventListener('click', resetGrid);
 
 
